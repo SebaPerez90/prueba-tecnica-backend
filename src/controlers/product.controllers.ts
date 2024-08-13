0322e14b455e134e6a3e4a9d048a106ff5f8869f
Sebastian Perez <112594443+SebaPerez90@users.noreply.github.com>
2024-08-13 12:18:47 -0300

    API products implementation

--- src/controlers/product.controllers.ts
+++ src/controlers/product.controllers.ts
@@ -1,12 +1,31 @@
-import { getProductByID, getAllProduct } from "../services/product.service";
+import { getByID, findAll } from "../services/product.service";
 import { Request, Response } from "express";
 
-export const getAllProductController = async (req: Request, res: Response) => {
+export const getAllProduct = async (req: Request, res: Response) => {
   try {
-    const products = await getAllProduct();
-    console.log(products);
+    const products = await findAll();
+    if (products?.length === 0) {
+      res.status(404).json({ message: "No products found" });
+    } else {
+      res.status(200).json(products);
+    }
+  } catch (error) {
+    res.status(500).json({ error: (error as Error).message });
+  }
+};
 
-    res.status(200).json(products);
+export const getProductByID = async (req: Request, res: Response) => {
+  try {
+    const parseID = parseInt(req.params.id);
+    if (isNaN(parseID)) {
+      res.status(400).json({
+        id: req.params.id,
+        message: "ID Must be a number. Please try again",
+      });
+    } else {
+      const product = await getByID(req.params.id);
+      res.status(200).json(product);
+    }
   } catch (error) {
     res.status(500).json({ error: (error as Error).message });
   }
--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,13 +1,20 @@
+import { IProduct } from "../dtos/product.dto";
 import { ENV } from "../environments";
 
-export const getProductByID = async (id: string) => {
-  const response = await fetch(`${ENV.API_URL_BASE}/products/${id}`);
-  const product = await response.json();
-  return product;
+export const findAll = async (): Promise<IProduct[] | undefined> => {
+  try {
+    const response = await fetch(`${ENV.API_URL_BASE}`);
+    if (response.ok) {
+      const products: IProduct[] = (await response.json()) as IProduct[];
+      return products;
+    }
+  } catch (error) {
+    throw new Error("Error fetching products");
+  }
 };
 
-export const getAllProduct = async () => {
-  const response = await fetch(`${ENV.API_URL_BASE}`);
-  const products = await response.json();
-  return products;
+export const getByID = async (id: string): Promise<IProduct> => {
+  const response = await fetch(`${ENV.API_URL_BASE}/${id}`);
+  const product: IProduct = (await response.json()) as IProduct;
+  return product;
 };
