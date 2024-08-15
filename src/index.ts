2e322eb4afbc013e40fcb7b440e2717921f95dbb
Sebastian Perez <112594443+SebaPerez90@users.noreply.github.com>
2024-08-15 14:29:46 -0300

    deploy configuration

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import app from "./app";
 import conectionDB from "./database/config/conectionDB";
 import { ENV } from "./environments";
 
+const PORT = process.env.PORT || 3000;
+
 conectionDB()
   .then(() =>
     app.listen(ENV.SERVER_PORT, () => {
-      console.log(`Server running on port ${ENV.SERVER_PORT}.`);
+      console.log(`Server running on port ${PORT}.`);
     })
   )
   .catch((error) => console.error(error));
