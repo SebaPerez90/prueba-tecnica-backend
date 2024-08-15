1901c1bdd6ef4b734cff88562b3d06e3e5c5ec9a
Sebastian Perez <112594443+SebaPerez90@users.noreply.github.com>
2024-08-15 14:18:55 -0300

    deploy configuration

--- src/routes/index.router.ts
+++ src/routes/index.router.ts
@@ -1,10 +1,13 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 import authRouter from "./auth.route";
 import usersRouter from "./users.route";
 import productRouter from "./products.route";
 
 const router = Router();
 
+router.get("/", (_req: Request, res: Response) => {
+  res.send("Swift Cart RestAPI");
+});
 router.use(authRouter);
 router.use(usersRouter);
 router.use(productRouter);
