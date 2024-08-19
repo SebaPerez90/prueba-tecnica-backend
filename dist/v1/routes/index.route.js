73936e33701f13f612f5294b9ecaaa1dc23c3924
Sebastian Perez <112594443+SebaPerez90@users.noreply.github.com>
2024-08-19 01:52:21 -0300

    CRUD & swagger config user logic

--- dist/v1/routes/index.route.js
+++ dist/v1/routes/index.route.js
@@ -9,7 +9,11 @@ const users_route_1 = __importDefault(require("./users.route"));
 const products_route_1 = __importDefault(require("./products.route"));
 const router = (0, express_1.Router)();
 router.get("/", (_req, res) => {
-    res.json("Welcome to SwiftCart REST API 🚀");
+    res.status(200).json({
+        message: "Welcome to SwiftCart REST API. Visit the documentation URL for more details: https://prueba-tecnica-backend.onrender.com/api/v1/docs/",
+        info: "Visit the documentation url for more details",
+        URL: "https://prueba-tecnica-backend.onrender.com/api/v1/docs/",
+    });
 });
 router.use(auth_route_1.default);
 router.use(users_route_1.default);
---
+++ dist/v1/swagger.components.js
@@ -0,0 +1,35 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.swaggerComponents = void 0;
+exports.swaggerComponents = {
+    schemas: {
+        ExampleProductDto: {
+            type: "object",
+            required: ["name", "description", "price", "stock", "imgUrl"],
+            properties: {
+                name: {
+                    type: "string",
+                    example: "Laptop",
+                },
+                description: {
+                    type: "string",
+                    example: "A high-end gaming laptop.",
+                },
+                price: {
+                    type: "number",
+                    format: "float",
+                    example: 999.99,
+                },
+                stock: {
+                    type: "integer",
+                    example: 100,
+                },
+                imgUrl: {
+                    type: "string",
+                    example: "http://example.com/image.png",
+                },
+            },
+        },
+    },
+};
+// to add schema in route add --> $ref: '#/components/schemas/Product'
